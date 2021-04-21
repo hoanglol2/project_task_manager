74b78813cb6d5bde5c40675e5755997f380a6918
hoanglol2 <duyhoangpt98@gmail.com>
2021-04-21 23:11:18 +0700

    first commit

---
+++ app/src/main/java/com/example/project_task_manager/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.project_task_manager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().apply {
+            add(android.R.id.content, BroadFragment(), "BoardFragment")
+            commit()
+        }
+    }
+}
